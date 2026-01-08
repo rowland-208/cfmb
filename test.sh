b3606183383ab49d2fdf8cf9a8ffcad9ed0dfd1d
rowland-208 <rowland.208@gmail.com>
2026-01-08 16:39:42 -0500

    Update tests to use uv

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 if [ ! -d venv ]; then
-    python -m venv venv
-    pip install -r requirements-test.txt
+    uv venv venv --python=python3
 fi
 source venv/bin/activate
+
+uv pip install -r requirements-test.txt
+
+set -o allexport
+source env/test.env
+set +o allexport
 
 pytest
